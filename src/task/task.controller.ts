79fd9eb4e4d602b53862ed59b2f1dd67c9a14db2
Jean C. Contreras <jean.cntrs@gmail.com>
2021-10-14 23:48:15 -0300

    Was added task creation

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,34 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TaskDTO } from './dto/task.dto';
+import { TaskService } from './task.service';
 
-@Controller('api/v1/task')
+@Controller('api/v1/tasks')
 export class TaskController {
-    @Get('done')
-    methodGet(
-        @Req() req: Request,
-        @Query('id') id: number,
-        @Query('description') description: number
-    ) {
-        return { method: req.method, id, description }
-    }
+    constructor(private readonly taskService: TaskService) { }
 
-    @Post(':id')
-    methodPost(@Req() req: Request, @Param('id') id: string) {
-        return { method: req.method, id }
-    }
-
-    @Put()
-    methodPut(@Req() req: Request, @Body() body: any) {
-        return { method: req.method, body }
-    }
-
-    @Patch()
-    methodPatch(@Req() req: Request) {
-        return `method ${req.method}`
-    }
-
-    @Delete()
-    methodDelete(@Req() req: Request) {
-        return `method ${req.method}`
+    @Post()
+    create(@Body() taskDTO: TaskDTO) {
+        return this.taskService.create(taskDTO)
     }
 }
